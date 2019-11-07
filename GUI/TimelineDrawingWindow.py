28376afb31fe241848733db818dc380fdbb6bb89
Pho Hale <PhoHale@gmail.com>
2019-11-07 13:21:04 -0500

    Got second working partitions track

--- GUI/TimelineDrawingWindow.py
+++ GUI/TimelineDrawingWindow.py
@@ -24,7 +24,8 @@ class TimelineDrawingWindow(QtWidgets.QMainWindow):
 
         self.totalStartTime = totalStartTime
         self.totalEndTime = totalEndTime
-
+        self.totalDuration = (self.totalEndTime - self.totalStartTime)
+    
         self.initUI()
 
 
@@ -55,16 +56,20 @@ class TimelineDrawingWindow(QtWidgets.QMainWindow):
         # Timeline Numberline track:
         self.timelineMasterTrackWidget = QTimeLine(360, desiredWindowWidth)
 
-        # Video Tracks
-        self.partitionsTrackWidget = TimelineTrackDrawingWidget_Partition(-1, None, [], self.totalStartTime, self.totalEndTime)
-        self.partitionsTrackWidget.selection_changed.connect(self.handle_child_selection_event)
-        self.partitionsTrackWidget.hover_changed.connect(self.handle_child_hover_event)
 
+        # Video Track
+        ## TODO: The video tracks must set:
+        # self.mainVideoTrack.selection_changed.connect(self.handle_child_selection_event)
+        # self.mainVideoTrack.hover_changed.connect(self.handle_child_hover_event)
+
+        
         # Other Tracks:
         self.eventTrackWidgets = []
 
-        # self.partitionsTwoTrackWidget = TimelineTrackDrawingWidget_Partition(0, None, [], self.totalStartTime, self.totalEndTime)
-        # self.eventTrackWidgets.append(self.partitionsTwoTrackWidget)
+        self.partitionsTrackWidget = TimelineTrackDrawingWidget_Partition(0, None, [], self.totalStartTime, self.totalEndTime)
+
+        self.partitionsTwoTrackWidget = TimelineTrackDrawingWidget_Partition(1, None, [], self.totalStartTime, self.totalEndTime)
+        self.eventTrackWidgets.append(self.partitionsTwoTrackWidget)
 
         # Build the bottomPanelWidget
         self.extendedTracksContainer = QtWidgets.QWidget()
@@ -87,6 +92,10 @@ class TimelineDrawingWindow(QtWidgets.QMainWindow):
         self.extendedTracksContainerVboxLayout.addWidget(self.partitionsTrackWidget)
         self.partitionsTrackWidget.setMinimumSize(500,50)
         self.partitionsTrackWidget.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+
+        # self.extendedTracksContainerVboxLayout.addWidget(self.partitionsTwoTrackWidget)
+        # self.partitionsTwoTrackWidget.setMinimumSize(500,50)
+        # self.partitionsTwoTrackWidget.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
 
         #Layout of Main Window:
 
@@ -122,7 +131,7 @@ class TimelineDrawingWindow(QtWidgets.QMainWindow):
         #self.cursorTraceRect = QRect(0,0,0,0)
 
 
-
+    # Timeline position/time converion functions:
     def offset_to_percent(self, event_x, event_y):
         percent_x = event_x / self.width()
         percent_y = event_y / self.height()
@@ -130,12 +139,13 @@ class TimelineDrawingWindow(QtWidgets.QMainWindow):
 
     def offset_to_duration(self, event_x):
         (percent_x, percent_y) = self.offset_to_percent(event_x, 0.0)
-        return (self.partitionsTrackWidget.totalDuration * percent_x)
+        return (self.totalDuration * percent_x)
 
     def offset_to_datetime(self, event_x):
         duration_offset = self.offset_to_duration(event_x)
-        return (self.partitionsTrackWidget.totalStartTime + duration_offset)
+        return (self.totalStartTime + duration_offset)
 
+    # Event Handlers:
     def keyPressEvent(self, event):
         self.partitionsTrackWidget.keyPressEvent(event)
 
@@ -145,6 +155,7 @@ class TimelineDrawingWindow(QtWidgets.QMainWindow):
         duration_offset = self.offset_to_duration(self.cursorX)
         datetime = self.offset_to_datetime(self.cursorX)
         text = "window x: {0},  duration: {1}, datetime: {2}".format(self.cursorX, duration_offset, datetime)
+        # Call the on_mouse_moved handler for the video track which will update its .hovered_object property, which is then read and used for relative offsets
         self.partitionsTrackWidget.on_mouse_moved(event)
         potentially_hovered_child_object = self.partitionsTrackWidget.hovered_object
         if potentially_hovered_child_object:
@@ -154,7 +165,7 @@ class TimelineDrawingWindow(QtWidgets.QMainWindow):
         self.statusBar().showMessage(text)
 
     # @pyqtSlot(int, int)
-    # Occurs when the user selects an object in the video track with the mouse
+    # Occurs when the user selects an object in the child video track with the mouse
     def handle_child_selection_event(self, trackIndex, trackObjectIndex):
         text = "handle_child_selection_event(...): trackIndex: {0}, trackObjectIndex: {1}".format(trackIndex, trackObjectIndex)
         # print(text)
@@ -173,7 +184,7 @@ class TimelineDrawingWindow(QtWidgets.QMainWindow):
                     currWidget.set_active_filter(currHoveredObject.startTime, currHoveredObject.endTime)
 
 
-    # Occurs when the user selects an object in the video track with the mouse
+    # Occurs when the user selects an object in the child video track with the mouse
     def handle_child_hover_event(self, trackIndex, trackObjectIndex):
         text = "handle_child_hover_event(...): trackIndex: {0}, trackObjectIndex: {1}".format(trackIndex, trackObjectIndex)
         # print(text)
