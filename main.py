0275f8b372393d01e29afa04794d3cbd3f7e38cb
Pho Hale <PhoHale@gmail.com>
2019-11-22 16:47:54 -0500

    Disabled example database table window

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ class TimelineApplication(QApplication):
     shouldShowGUIWindows = True
     shouldShowMainGUIWindow = True
     shouldShowListGUIWindow = False
-    shouldShowExampleWindow = True
+    shouldShowExampleWindow = False
 
     def __init__(self, args):
         super(TimelineApplication, self).__init__(args)
-        self.database_file_path = '/Users/pho/repo/PhoPyQtTimelinePlotter/BehavioralBoxDatabase.db'
+        # self.database_file_path = '/Users/pho/repo/PhoPyQtTimelinePlotter/BehavioralBoxDatabase.db'
         # self.database_file_path = 'G:\Google Drive\Modern Behavior Box\Results - Data\BehavioralBoxDatabase.db'
-        # self.database_file_path = "C:/Users/halechr/repo/PhoPyQtTimelinePlotter/BehavioralBoxDatabase.db"
+        self.database_file_path = "C:/Users/halechr/repo/PhoPyQtTimelinePlotter/BehavioralBoxDatabase.db"
         self.database_connection = DatabaseConnectionRef(self.database_file_path)
 
         # Show last 7 days worth of data
