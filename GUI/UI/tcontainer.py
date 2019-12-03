8e8f5501cf84d7e82ec74fe3b12460adf0e33b14
Pho Hale <PhoHale@gmail.com>
2019-12-03 03:44:15 -0500

    Temp

--- GUI/UI/tcontainer.py
+++ GUI/UI/tcontainer.py
@@ -19,7 +19,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QMenu, QLabel, Q
 "T"/"B": Top/Bottom
 "L"/"R": Left/Right
 """
-class Mode(Enum):
+class ResizableContainerMode(Enum):
     NONE = 0,
     MOVE = 1,
     RESIZETL = 2,
@@ -31,56 +31,28 @@ class Mode(Enum):
     RESIZEBL = 8,
     RESIZEL = 9
 
-"""
-TContainer: a freely resizable container widget that embeds its main contents in a frame that allows the user to resize (by producing handles at the edges of the widget), drag, etc
-- TODO: need to add a "ModeMask" like variable that disables certain resizing actions (like resizing from any of the corners, in a certain dimension, etc).
-    - If it detects a mode that isn't permitted in the mask, it returns self.mode = None
-"""
-class TContainer(QWidget):
-    """ allow to move and resize by user"""
-    menu = None
-    mode = Mode.NONE
+
+class ResizableContainerWidgetMixin(object):
+    mode = ResizableContainerMode.NONE
     position = None
     inFocus = pyqtSignal(bool)
     outFocus = pyqtSignal(bool)
     newGeometry = pyqtSignal(QRect)
 
-    def __init__(self, parent, p, cWidget):
-        super().__init__(parent=parent)
-
-        self.menu = QMenu(parent=self, title='menu')
-        self.setAttribute(QtCore.Qt.WA_DeleteOnClose, True)
+    def init_resizable(self, parent):
         self.setVisible(True)
         self.setAutoFillBackground(False)
         self.setMouseTracking(True)
         self.setFocusPolicy(QtCore.Qt.ClickFocus)
         self.setFocus()
-        self.move(p)
+        # self.move(p)
 
-        self.vLayout = QVBoxLayout(self)
-        self.setChildWidget(cWidget)
+        # self.vLayout = QVBoxLayout(self)
+        # self.setChildWidget(cWidget)
 
         self.m_infocus = True
-        self.m_showMenu = False
         self.m_isEditing = True
         self.installEventFilter(parent)
-
-    def setChildWidget(self, cWidget):
-        if cWidget:
-            self.childWidget = cWidget
-            self.childWidget.setAttribute(QtCore.Qt.WA_TransparentForMouseEvents, True)
-            self.childWidget.setParent(self)
-            self.childWidget.releaseMouse()
-            self.vLayout.addWidget(cWidget)
-            self.vLayout.setContentsMargins(0,0,0,0)
-
-    def popupShow(self, pt: QPoint):
-        if self.menu.isEmpty:
-            return
-        global_ = self.mapToGlobal(pt)
-        self.m_showMenu = True
-        self.menu.exec(global_)
-        self.m_showMenu = False
 
     def focusInEvent(self, a0: QtGui.QFocusEvent):
         self.m_infocus = True
@@ -94,36 +66,71 @@ class TContainer(QWidget):
             return
         if self.m_showMenu:
             return
-        self.mode = Mode.NONE
+        self.mode = ResizableContainerMode.NONE
         self.outFocus.emit(False)
         self.m_infocus = False
 
-    def paintEvent(self, e: QtGui.QPaintEvent):
-        painter = QtGui.QPainter(self)
-        color = (r, g, b, a) = (255, 0, 0, 16)
-        painter.fillRect(e.rect(), QColor(r, g, b, a))
+    # It seems most of the magic is being done here. This function sets the cursor shape (whether it's a "resizing" handle or a mouse) and sets the "mode" variable to indicate which mode it's currently in.,
+    def setCursorShape(self, e_pos: QPoint):
+        diff = 3
+        # Left - Bottom
 
-        if self.m_infocus:
-            rect = e.rect()
-            rect.adjust(0,0,-1,-1)
-            painter.setPen(QColor(r, g, b))
-            painter.drawRect(rect)
+        if (((e_pos.y() > self.y() + self.height() - diff) and # Bottom
+            (e_pos.x() < self.x() + diff)) or # Left
+        # Right-Bottom
+        ((e_pos.y() > self.y() + self.height() - diff) and # Bottom
+        (e_pos.x() > self.x() + self.width() - diff)) or # Right
+        # Left-Top
+        ((e_pos.y() < self.y() + diff) and # Top
+        (e_pos.x() < self.x() + diff)) or # Left
+        # Right-Top
+        (e_pos.y() < self.y() + diff) and # Top
+        (e_pos.x() > self.x() + self.width() - diff)): # Right
+            # Left - Bottom
+            if ((e_pos.y() > self.y() + self.height() - diff) and # Bottom
+            (e_pos.x() < self.x()
+                + diff)): # Left
+                self.mode = ResizableContainerMode.RESIZEBL
+                self.setCursor(QCursor(QtCore.Qt.SizeBDiagCursor))
+                # Right - Bottom
+            if ((e_pos.y() > self.y() + self.height() - diff) and # Bottom
+            (e_pos.x() > self.x() + self.width() - diff)): # Right
+                self.mode = ResizableContainerMode.RESIZEBR
+                self.setCursor(QCursor(QtCore.Qt.SizeFDiagCursor))
+            # Left - Top
+            if ((e_pos.y() < self.y() + diff) and # Top
+            (e_pos.x() < self.x() + diff)): # Left
+                self.mode = ResizableContainerMode.RESIZETL
+                self.setCursor(QCursor(QtCore.Qt.SizeFDiagCursor))
+            # Right - Top
+            if ((e_pos.y() < self.y() + diff) and # Top
+            (e_pos.x() > self.x() + self.width() - diff)): # Right
+                self.mode = ResizableContainerMode.RESIZETR
+                self.setCursor(QCursor(QtCore.Qt.SizeBDiagCursor))
+        # check cursor horizontal position
+        elif ((e_pos.x() < self.x() + diff) or # Left
+            (e_pos.x() > self.x() + self.width() - diff)): # Right
+            if e_pos.x() < self.x() + diff: # Left
+                self.setCursor(QCursor(QtCore.Qt.SizeHorCursor))
+                self.mode = ResizableContainerMode.RESIZEL
+            else: # Right
+                self.setCursor(QCursor(QtCore.Qt.SizeHorCursor))
+                self.mode = ResizableContainerMode.RESIZER
+        # check cursor vertical position
+        elif ((e_pos.y() > self.y() + self.height() - diff) or # Bottom
+            (e_pos.y() < self.y() + diff)): # Top
+            if e_pos.y() < self.y() + diff: # Top
+                self.setCursor(QCursor(QtCore.Qt.SizeVerCursor))
+                self.mode = ResizableContainerMode.RESIZET
+            else: # Bottom
+                self.setCursor(QCursor(QtCore.Qt.SizeVerCursor))
+                self.mode = ResizableContainerMode.RESIZEB
+        else:
+            self.setCursor(QCursor(QtCore.Qt. ArrowCursor))
+            self.mode = ResizableContainerMode.MOVE
 
 
-    def mousePressEvent(self, e: QtGui.QMouseEvent):
-        self.position = QPoint(e.globalX() - self.geometry().x(), e.globalY() - self.geometry().y())
-        if not self.m_isEditing:
-            return
-        if not self.m_infocus:
-            return
-        if not e.buttons() and QtCore.Qt.LeftButton:
-            self.setCursorShape(e.pos())
-            return
-        if e.button() == QtCore.Qt.RightButton:
-            self.popupShow(e.pos())
-            e.accept()
-
-    def keyPressEvent(self, e: QtGui.QKeyEvent):
+    def resizableContainer_keyPressEvent(self, e: QtGui.QKeyEvent):
         if not self.m_isEditing: return
         if e.key() == QtCore.Qt.Key_Delete:
             self.deleteLater()
@@ -151,72 +158,20 @@ class TContainer(QWidget):
                 self.resize(self.width() + 1, self.height())
         self.newGeometry.emit(self.geometry())
 
-    # It seems most of the magic is being done here. This function sets the cursor shape (whether it's a "resizing" handle or a mouse) and sets the "mode" variable to indicate which mode it's currently in.,
-    def setCursorShape(self, e_pos: QPoint):
-        diff = 3
-        # Left - Bottom
 
-        if (((e_pos.y() > self.y() + self.height() - diff) and # Bottom
-            (e_pos.x() < self.x() + diff)) or # Left
-        # Right-Bottom
-        ((e_pos.y() > self.y() + self.height() - diff) and # Bottom
-        (e_pos.x() > self.x() + self.width() - diff)) or # Right
-        # Left-Top
-        ((e_pos.y() < self.y() + diff) and # Top
-        (e_pos.x() < self.x() + diff)) or # Left
-        # Right-Top
-        (e_pos.y() < self.y() + diff) and # Top
-        (e_pos.x() > self.x() + self.width() - diff)): # Right
-            # Left - Bottom
-            if ((e_pos.y() > self.y() + self.height() - diff) and # Bottom
-            (e_pos.x() < self.x()
-                + diff)): # Left
-                self.mode = Mode.RESIZEBL
-                self.setCursor(QCursor(QtCore.Qt.SizeBDiagCursor))
-                # Right - Bottom
-            if ((e_pos.y() > self.y() + self.height() - diff) and # Bottom
-            (e_pos.x() > self.x() + self.width() - diff)): # Right
-                self.mode = Mode.RESIZEBR
-                self.setCursor(QCursor(QtCore.Qt.SizeFDiagCursor))
-            # Left - Top
-            if ((e_pos.y() < self.y() + diff) and # Top
-            (e_pos.x() < self.x() + diff)): # Left
-                self.mode = Mode.RESIZETL
-                self.setCursor(QCursor(QtCore.Qt.SizeFDiagCursor))
-            # Right - Top
-            if ((e_pos.y() < self.y() + diff) and # Top
-            (e_pos.x() > self.x() + self.width() - diff)): # Right
-                self.mode = Mode.RESIZETR
-                self.setCursor(QCursor(QtCore.Qt.SizeBDiagCursor))
-        # check cursor horizontal position
-        elif ((e_pos.x() < self.x() + diff) or # Left
-            (e_pos.x() > self.x() + self.width() - diff)): # Right
-            if e_pos.x() < self.x() + diff: # Left
-                self.setCursor(QCursor(QtCore.Qt.SizeHorCursor))
-                self.mode = Mode.RESIZEL
-            else: # Right
-                self.setCursor(QCursor(QtCore.Qt.SizeHorCursor))
-                self.mode = Mode.RESIZER
-        # check cursor vertical position
-        elif ((e_pos.y() > self.y() + self.height() - diff) or # Bottom
-            (e_pos.y() < self.y() + diff)): # Top
-            if e_pos.y() < self.y() + diff: # Top
-                self.setCursor(QCursor(QtCore.Qt.SizeVerCursor))
-                self.mode = Mode.RESIZET
-            else: # Bottom
-                self.setCursor(QCursor(QtCore.Qt.SizeVerCursor))
-                self.mode = Mode.RESIZEB
-        else:
-            self.setCursor(QCursor(QtCore.Qt. ArrowCursor))
-            self.mode = Mode.MOVE
+    def resizableContainer_mousePressEvent(self, e: QtGui.QMouseEvent):
+        self.position = QPoint(e.globalX() - self.geometry().x(), e.globalY() - self.geometry().y())
+        if not self.m_isEditing:
+            return False
+        if not self.m_infocus:
+            return False
+        if not e.buttons() and QtCore.Qt.LeftButton:
+            self.setCursorShape(e.pos())
+            return False
+        return True
 
 
-    def mouseReleaseEvent(self, e: QtGui.QMouseEvent):
-        QWidget.mouseReleaseEvent(self, e)
-
-
-    def mouseMoveEvent(self, e: QtGui.QMouseEvent):
-        QWidget.mouseMoveEvent(self, e)
+    def resizableContainer_mouseMoveEvent(self, e: QtGui.QMouseEvent):
         if not self.m_isEditing:
             return
         if not self.m_infocus:
@@ -226,7 +181,7 @@ class TContainer(QWidget):
             self.setCursorShape(p)
             return
 
-        if (self.mode == Mode.MOVE or self.mode == Mode.NONE) and e.buttons() and QtCore.Qt.LeftButton:
+        if (self.mode == ResizableContainerMode.MOVE or self.mode == ResizableContainerMode.NONE) and e.buttons() and QtCore.Qt.LeftButton:
             toMove = e.globalPos() - self.position
             if toMove.x() < 0:return
             if toMove.y() < 0:return
@@ -235,41 +190,123 @@ class TContainer(QWidget):
             self.newGeometry.emit(self.geometry())
             self.parentWidget().repaint()
             return
-        if (self.mode != Mode.MOVE) and e.buttons() and QtCore.Qt.LeftButton:
-            if self.mode == Mode.RESIZETL: # Left - Top
+        if (self.mode != ResizableContainerMode.MOVE) and e.buttons() and QtCore.Qt.LeftButton:
+            if self.mode == ResizableContainerMode.RESIZETL: # Left - Top
                 newwidth = e.globalX() - self.position.x() - self.geometry().x()
                 newheight = e.globalY() - self.position.y() - self.geometry().y()
                 toMove = e.globalPos() - self.position
                 self.resize(self.geometry().width() - newwidth, self.geometry().height() - newheight)
                 self.move(toMove.x(), toMove.y())
-            elif self.mode == Mode.RESIZETR: # Right - Top
+            elif self.mode == ResizableContainerMode.RESIZETR: # Right - Top
                 newheight = e.globalY() - self.position.y() - self.geometry().y()
                 toMove = e.globalPos() - self.position
                 self.resize(e.x(), self.geometry().height() - newheight)
                 self.move(self.x(), toMove.y())
-            elif self.mode== Mode.RESIZEBL: # Left - Bottom
+            elif self.mode== ResizableContainerMode.RESIZEBL: # Left - Bottom
                 newwidth = e.globalX() - self.position.x() - self.geometry().x()
                 toMove = e.globalPos() - self.position
                 self.resize(self.geometry().width() - newwidth, e.y())
                 self.move(toMove.x(), self.y())
-            elif self.mode == Mode.RESIZEB: # Bottom
+            elif self.mode == ResizableContainerMode.RESIZEB: # Bottom
                 self.resize(self.width(), e.y())
-            elif self.mode == Mode.RESIZEL: # Left
+            elif self.mode == ResizableContainerMode.RESIZEL: # Left
                 newwidth = e.globalX() - self.position.x() - self.geometry().x()
                 toMove = e.globalPos() - self.position
                 self.resize(self.geometry().width() - newwidth, self.height())
                 self.move(toMove.x(), self.y())
-            elif self.mode == Mode.RESIZET:# Top
+            elif self.mode == ResizableContainerMode.RESIZET:# Top
                 newheight = e.globalY() - self.position.y() - self.geometry().y()
                 toMove = e.globalPos() - self.position
                 self.resize(self.width(), self.geometry().height() - newheight)
                 self.move(self.x(), toMove.y())
-            elif self.mode == Mode.RESIZER: # Right
+            elif self.mode == ResizableContainerMode.RESIZER: # Right
                 self.resize(e.x(), self.height())
-            elif self.mode == Mode.RESIZEBR:# Right - Bottom
+            elif self.mode == ResizableContainerMode.RESIZEBR:# Right - Bottom
                 self.resize(e.x(), e.y())
             self.parentWidget().repaint()
         self.newGeometry.emit(self.geometry())
+
+
+"""
+TContainer: a freely resizable container widget that embeds its main contents in a frame that allows the user to resize (by producing handles at the edges of the widget), drag, etc
+- TODO: need to add a "ModeMask" like variable that disables certain resizing actions (like resizing from any of the corners, in a certain dimension, etc).
+    - If it detects a mode that isn't permitted in the mask, it returns self.mode = None
+"""
+class TContainer(ResizableContainerWidgetMixin, QWidget):
+    """ allow to move and resize by user"""
+    menu = None
+    # mode = ResizableContainerMode.NONE
+    # position = None
+    # inFocus = pyqtSignal(bool)
+    # outFocus = pyqtSignal(bool)
+    # newGeometry = pyqtSignal(QRect)
+
+    def __init__(self, parent, p, cWidget):
+        super().__init__(parent=parent)
+    
+        self.menu = QMenu(parent=self, title='menu')
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose, True)
+        # self.setVisible(True)
+        # self.setAutoFillBackground(False)
+        # self.setMouseTracking(True)
+        # self.setFocusPolicy(QtCore.Qt.ClickFocus)
+        # self.setFocus()
+        self.move(p)
+
+        self.vLayout = QVBoxLayout(self)
+        self.setChildWidget(cWidget)
+
+        # self.m_infocus = True
+        # self.m_showMenu = False
+        # self.m_isEditing = True
+        # self.installEventFilter(parent)
+        self.init_resizable(parent)
+
+    def setChildWidget(self, cWidget):
+        if cWidget:
+            self.childWidget = cWidget
+            self.childWidget.setAttribute(QtCore.Qt.WA_TransparentForMouseEvents, True)
+            self.childWidget.setParent(self)
+            self.childWidget.releaseMouse()
+            self.vLayout.addWidget(cWidget)
+            self.vLayout.setContentsMargins(0,0,0,0)
+
+    def popupShow(self, pt: QPoint):
+        if self.menu.isEmpty:
+            return
+        global_ = self.mapToGlobal(pt)
+        self.m_showMenu = True
+        self.menu.exec(global_)
+        self.m_showMenu = False
+
+    def paintEvent(self, e: QtGui.QPaintEvent):
+        painter = QtGui.QPainter(self)
+        color = (r, g, b, a) = (255, 0, 0, 16)
+        painter.fillRect(e.rect(), QColor(r, g, b, a))
+
+        if self.m_infocus:
+            rect = e.rect()
+            rect.adjust(0,0,-1,-1)
+            painter.setPen(QColor(r, g, b))
+            painter.drawRect(rect)
+
+
+    def mousePressEvent(self, e: QtGui.QMouseEvent):
+        if (self.resizableContainer_mousePressEvent(e)):
+            if e.button() == QtCore.Qt.RightButton:
+                self.popupShow(e.pos())
+                e.accept()
+
+    def keyPressEvent(self, e: QtGui.QKeyEvent):
+        self.resizableContainer_keyPressEvent(e)
+
+    def mouseReleaseEvent(self, e: QtGui.QMouseEvent):
+        QWidget.mouseReleaseEvent(self, e)
+
+    def mouseMoveEvent(self, e: QtGui.QMouseEvent):
+        QWidget.mouseMoveEvent(self, e)
+        self.resizableContainer_mouseMoveEvent(e)
+
 
 
 class MainWindow(QMainWindow):
@@ -280,9 +317,9 @@ class MainWindow(QMainWindow):
         lab1 = QLabel("Label1")
         lab2 = QLabel("Label2")
         con1 = TContainer(self, QPoint(10,10), lab1)
-        con1.mode = Mode.RESIZER
+        con1.mode = ResizableContainerMode.RESIZER
         con2 = TContainer(self, QPoint(20,50), lab2)
-        con2.mode = Mode.NONE
+        con2.mode = ResizableContainerMode.NONE
 
 
 if __name__ == '__main__':
