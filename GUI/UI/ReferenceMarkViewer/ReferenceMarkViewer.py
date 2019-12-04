e979fafb9f68ccb3ba28335429292d9286931532
Pho Hale <PhoHale@gmail.com>
2019-12-04 13:56:17 -0500

    Renamed the ReferenceMarkViewer

--- GUI/UI/ReferenceMarkViewer/ReferenceMarkViewer.py
+++ GUI/UI/ReferenceMarkViewer/ReferenceMarkViewer.py
@@ -10,26 +10,32 @@ from PyQt5.QtGui import QPainter, QBrush, QPen, QColor, QFont, QIcon
 from PyQt5.QtCore import Qt, QPoint, QRect, QObject, QEvent, pyqtSignal, pyqtSlot, QSize, QDir
 
 
-class ReferenceMarkViewer(QtWidgets.QDockWidget):
 
-     # This defines a signal called 'closed' that takes no arguments.
-    on_cancel = pyqtSignal()
-
-     # This defines a signal called 'closed' that takes no arguments.
-    # on_commit = pyqtSignal([datetime, str, str, str], [datetime, datetime, str, str, str])
-
-    on_commit = pyqtSignal([datetime, str, str, str, int, int, int, int], [datetime, datetime, str, str, str, int, int, int, int])
+class ReferenceMarkViewer(QWidget):
 
 
     def __init__(self, parent=None):
         super(ReferenceMarkViewer, self).__init__(parent=parent) # Call the inherited classes __init__ method
-        self.ui = uic.loadUi("GUI/UI/ReferenceMarkViewer/TeReferenceMarkViewerxtAnnotations.ui", self) # Load the .ui file
+        self.ui = uic.loadUi("GUI/UI/ReferenceMarkViewer/ReferenceMarkViewer.ui", self) # Load the .ui file
         self.initUI()
         self.show() # Show the GUI
 
 
     def initUI(self):
-        # self.ui.buttonBox.accepted.connect(self.accept)
-        # self.ui.buttonBox.rejected.connect(self.reject)
+        pass
+
+
+
+
+class ReferenceMarkViewer_DockWidget(QtWidgets.QDockWidget):
+
+    def __init__(self, parent=None):
+        super(ReferenceMarkViewer_DockWidget, self).__init__(parent=parent) # Call the inherited classes __init__ method
+        self.ui = uic.loadUi("GUI/UI/ReferenceMarkViewer/DockWidget_ReferenceMarkViewer.ui", self) # Load the .ui file
+        self.initUI()
+        self.show() # Show the GUI
+
+
+    def initUI(self):
         pass
 
