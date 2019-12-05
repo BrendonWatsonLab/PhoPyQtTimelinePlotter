29ee3eea75ea1688b00733f0bb9424e7ccf2f923
Pho Hale <PhoHale@gmail.com>
2019-12-05 18:22:59 -0500

    Added "RepresentedTimeRange" class that holds the startTime, endTime properties and emits a signal only when the values change.

--- GUI/Helpers/DurationRepresentationHelpers.py
+++ GUI/Helpers/DurationRepresentationHelpers.py
@@ -1,4 +1,59 @@
+# -*- coding: utf-8 -*-
 # DurationRepresentationHelpers.py
+import sys
+from enum import Enum
+from datetime import datetime, timezone, timedelta
+from PyQt5.QtWidgets import QFrame
+from PyQt5.QtCore import Qt, QPoint, QRect, QObject, QEvent, pyqtSignal, pyqtSlot, QSize
+
+
+"""
+RepresentedTimeRange: on object that holds a reference to the current global start and end times
+"""
+class RepresentedTimeRange(QObject):
+
+    timesChanged = pyqtSignal(datetime, datetime, timedelta) # Called when the timeline's global displayed start/end times are updated
+    
+    def __init__(self, totalStartTime, totalEndTime, parent=None):
+        super(RepresentedTimeRange, self).__init__(parent=parent)
+        self._totalStartTime = totalStartTime
+        self._totalEndTime = totalEndTime
+        self._totalDuration = (totalEndTime - totalStartTime)
+
+    # Getters:
+    @property
+    def totalStartTime(self):
+        return self._totalStartTime
+
+    @property
+    def totalEndTime(self):
+        return self._totalEndTime
+
+    @property
+    def totalDuration(self):
+        return (self.totalEndTime - self.totalStartTime)
+
+    # Setters:
+    @totalStartTime.setter
+    def totalStartTime(self, new_value):
+        if self._totalStartTime != new_value:
+            self._totalStartTime = new_value
+            self.timesChanged.emit(self.totalStartTime, self.totalEndTime, self.totalDuration)
+        else:
+            # Otherwise nothing has changed
+            pass
+
+    @totalEndTime.setter
+    def totalEndTime(self, new_value):
+        if self._totalEndTime != new_value:
+            self._totalEndTime = new_value
+            self.timesChanged.emit(self.totalStartTime, self.totalEndTime, self.totalDuration)
+        else:
+            # Otherwise nothing has changed
+            pass
+
+
+
 
 
 ## IMPORTS:
