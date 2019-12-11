4e5b8e3db5ef8d8414ee786760582f060ad78cca
Pho Hale <PhoHale@gmail.com>
2019-12-11 09:03:42 -0500

    Made datetime string generation functions cross-platform (only tested on Windows)

--- GUI/UI/TickedTimelineDrawingBaseWidget.py
+++ GUI/UI/TickedTimelineDrawingBaseWidget.py
@@ -176,7 +176,9 @@ class TickedTimelineDrawingBaseWidget(FixedTimelineContentsWidthMixin, QWidget):
     def get_time_string(self, seconds):
         drawTime = self.totalStartTime + timedelta(seconds=seconds)
         # return drawTime.strftime("%d-%m-%Y %I %p")
-        return drawTime.strftime("%#m/%#d \n%#I%p")
+        # return drawTime.strftime("%#m/%#d \n%#I%p") # Works on windows
+        return drawTime.strftime("X%m/X%d \nX%I%p").replace('X0', 'X').replace('X', '')
+
         # m, s = divmod(seconds, 60)
         # h, m = divmod(m, 60)
         # return "%02d:%02d:%02d" % (h, m, s)
@@ -184,11 +186,14 @@ class TickedTimelineDrawingBaseWidget(FixedTimelineContentsWidthMixin, QWidget):
 
     # Get "7/27" style timestring for the start of each day
     def get_start_of_day_time_string(self, start_of_day_date):
-        return start_of_day_date.strftime("%#m/%#d")
+        # return start_of_day_date.strftime("%#m/%#d") # Works on Windows
+        return start_of_day_date.strftime("X%m/X%d").replace('X0', 'X').replace('X', '')
+
 
     # Get time string from seconds
     def get_hours_of_day_only_time_string(self, drawTime):
-        return drawTime.strftime("%#I%p")
+        # return drawTime.strftime("%#I%p") # Works on Windows
+        return drawTime.strftime("X%I%p").replace('X0', 'X').replace('X', '')
 
 
     # Get scale from length
