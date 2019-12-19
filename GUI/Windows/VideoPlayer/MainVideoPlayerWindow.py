c4f6d900968d12fd52565b2e383cea2de2c5c994
Pho Hale <PhoHale@gmail.com>
2019-12-18 19:34:46 -0500

    Fixed bug with range

--- GUI/Windows/VideoPlayer/MainVideoPlayerWindow.py
+++ GUI/Windows/VideoPlayer/MainVideoPlayerWindow.py
@@ -146,7 +146,7 @@ class HistoricalFrameRenderingMixin(object):
                 earliest_desired_thumbnail_frame = min((curr_playback_frame - previousFrames), 0)
 
                 # note, this excludes curr_playback_frame because it excludes the end value
-                desired_thumbnail_indicies = range(earliest_desired_thumbnail_frame, curr_playback_frame, num_frames_step)
+                desired_thumbnail_indicies = list(range(earliest_desired_thumbnail_frame, curr_playback_frame, num_frames_step))
                 desired_thumbnail_indicies.append(curr_playback_frame)
 
                 self.generate_thumbnails(desired_thumbnail_indicies)
