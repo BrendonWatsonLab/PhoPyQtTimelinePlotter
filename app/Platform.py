3c0286318ad767554d7c770e20fb50fe7d851536
Pho Hale <PhoHale@gmail.com>
2020-06-04 07:32:12 -0400

    Added basic config file reader/writer

--- app/Platform.py
+++ app/Platform.py
@@ -1,8 +1,44 @@
 # Platform.py
 import pathlib
 from enum import Enum
+import configparser
 
 from PyQt5.QtCore import Qt, QObject, QDir
+
+# Manages loading/saving values to a .ini configuration file
+class ConfigManager(object):
+
+    def __init__(self, config_path='data/config.ini', parent=None):
+        # super().__init__(parent=parent)
+        super().__init__()
+        self.config_file_path = config_path
+        self.config = configparser.ConfigParser()
+        self.read_from_file()
+
+    def get_config(self):
+        return self.config
+        
+    def create_sample_config(self):
+        self.config['DEFAULT'] = {'ProjectDirectory': "C:/Users/halechr/repo/PhoPyQtTimelinePlotter/",
+            'FFMPEGPath': "EXTERNAL/Dependencies/ffmpeg/bin",
+            'Compression': 'yes'}
+
+
+            # config['DEFAULT']['ProjectDirectory']
+            # config['DEFAULT']['FFMPEGPath']
+        
+    def read_from_file(self, override_path = None):
+        if override_path is not None:
+            self.config_file_path = override_path
+
+        self.config.read(self.config_file_path)   
+
+    def write_to_file(self, override_path = None):
+        if override_path is not None:
+            self.config_file_path = override_path
+
+        with open(self.config_file_path, 'w') as configfile:
+            self.config.write(configfile)
 
 # A variable the holds the configuration for the current operating system
 class PlatformOperatingSystem(Enum):
@@ -12,10 +48,17 @@ class PlatformOperatingSystem(Enum):
     Unknown = 4
 
     def get_project_directory(self):
+        currConfigManagerConfig = ConfigManager().get_config()
+        # currConfigFileSpecified_Project_dir = currConfigManagerConfig['DEFAULT']['ProjectDirectory']
+        
+        # TODO: I think when the config file contains an empty string, the potential_path returns pathlib.Path('.') which might be exactly what we want.
+        # As a result, I don't think this is ever ran.
         if (self is PlatformOperatingSystem.Windows):
-            return pathlib.Path("C:/Users/halechr/repo/PhoPyQtTimelinePlotter/")
+            # return pathlib.Path("C:/Users/halechr/repo/PhoPyQtTimelinePlotter/")
+            fallback_path = "C:/Users/halechr/repo/PhoPyQtTimelinePlotter/"
         elif (self is PlatformOperatingSystem.Mac):
-            return pathlib.Path("/Users/pho/repo/PhoPyQtTimelinePlotter/")
+            # return pathlib.Path("/Users/pho/repo/PhoPyQtTimelinePlotter/")
+            fallback_path = "/Users/pho/repo/PhoPyQtTimelinePlotter/"
         elif (self is PlatformOperatingSystem.Linux):
             print("ERROR: Linux not implemented, set the path in main.py")
             raise NotImplementedError
@@ -24,6 +67,14 @@ class PlatformOperatingSystem(Enum):
             print("ERROR: Unknown Type")
             raise NotImplementedError
             return None
+
+        currConfigFileSpecified_Project_dir = currConfigManagerConfig.get('DEFAULT', 'ProjectDirectory', fallback=fallback_path)
+        # print('found proj dir: {} from config'.format(currConfigFileSpecified_Project_dir))
+        if currConfigFileSpecified_Project_dir is not None:
+            potential_path = pathlib.Path(currConfigFileSpecified_Project_dir)
+            # print('    potential_path: {}'.format(potential_path))
+            return potential_path
+
 
 
 # PlatformConfiguration: contains parameters specific to the current running instance of the program, like the project path, the OS it's running on (Mac/Win/Linux), etc.
@@ -69,7 +120,13 @@ class PlatformConfiguration(object):
             PlatformConfiguration.project_directory = new_user_dir
 
         # Using relative ffmpeg path:
-        PlatformConfiguration.ffmpeg_directory = PlatformConfiguration.project_directory.joinpath("EXTERNAL", "Dependencies", "ffmpeg", "bin")
+        currConfigManagerConfig = ConfigManager().get_config()
+        # currConfigFileSpecified_FFMPEG_relative_path = currConfigManagerConfig['DEFAULT']['FFMPEGPath']
+        currConfigFileSpecified_FFMPEG_relative_path = currConfigManagerConfig.get('DEFAULT', 'FFMPEGPath', fallback='EXTERNAL/Dependencies/ffmpeg/bin')
+        PlatformConfiguration.ffmpeg_directory = PlatformConfiguration.project_directory.joinpath(currConfigFileSpecified_FFMPEG_relative_path)
+
+        # PlatformConfiguration.ffmpeg_directory = PlatformConfiguration.project_directory.joinpath("EXTERNAL", "Dependencies", "ffmpeg", "bin")
+        
         if (PlatformConfiguration.ffmpeg_directory.exists()):
             PlatformConfiguration.ffprobe_executable_path_string = str(PlatformConfiguration.ffmpeg_directory.joinpath("ffprobe"))
         else:
